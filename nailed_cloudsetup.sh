839690d696d26241c487c02360cd06f3e2c8acdc
Maximilian Meister <mmeister@suse.de>
2015-03-05 11:53:24 +0100

    update cloud init

--- nailed_cloudsetup.sh
+++ nailed_cloudsetup.sh
@@ -2,8 +2,9 @@
 
 zypper --non-interactive --gpg-auto-import-keys --no-gpg-checks refresh -f
 zypper --non-interactive up -l
-zypper --non-interactive --gpg-auto-import-keys --no-gpg-checks install git sqlite3-devel gcc make ruby-devel libxml2-devel
+zypper --non-interactive --gpg-auto-import-keys --no-gpg-checks install git sqlite3-devel gcc make ruby-devel libxml2-devel rubygem-bundler libxslt-devel
 cd ~
 git clone https://github.com/MaximilianMeister/nailed.git
 cd ~/nailed
-bundle.ruby2.1 install
+bundle config build.nokogiri --use-system-libraries
+bundle install
